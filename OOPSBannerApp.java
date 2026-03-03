2a1bfac50f973a1eb3b8a1e4ba7bfaa433eaf6d5
Chanda <vc4533@srmist.edu.in>
2026-03-03 18:42:55 +0530

    UC2: Display OOPS in 7-line banner format using * and spaces

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,22 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC2: Display "OOPS" in Banner format using * and spaces
+ *
+ * @author Likhith
+ * @version 2.0
+ */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" *****    *****    *****    ***** ");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println("*     *  *     *  *     *  *     *");
+        System.out.println(" *****    *****    *****    ***** ");
+
     }
 }
